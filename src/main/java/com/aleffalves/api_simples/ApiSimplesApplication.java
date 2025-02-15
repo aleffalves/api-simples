0857ce18809a70eb722534920cb2307d3ce09f21
aleff-alves <aleff.alves@tangerino.com.br>
2025-02-15 10:40:39 -0300

    first commit

---
+++ src/main/java/com/aleffalves/api_simples/ApiSimplesApplication.java
@@ -0,0 +1,13 @@
+package com.aleffalves.api_simples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSimplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSimplesApplication.class, args);
+	}
+
+}
